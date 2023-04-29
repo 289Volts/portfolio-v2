609ffd9b3cc2a32b5fb7704b92f1f03edaa41e43
289volts <289volts@gmail.com>
2023-04-29 18:35:50 +0100

    .

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,9 +4,10 @@ module.exports = {
 	i18n: {
 		locales: ["en", "fr", "es", "de"],
 		defaultLocale: "en",
-		localePath: path.resolve("./public/locales"),
+		// localePath: path.resolve("./public/locales"),
 		localeDetection: false,
+		localeExtension: "json",
 		reloadOnPrerender: true,
-		ns: ["common"],
+		defaultNs: ["common"],
 	},
 };
